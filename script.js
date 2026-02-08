dc10fe6f5ac0f9107cf1b95329a9a579f3573200
jz7263628-alt <jz7263628@gmail.com>
2026-02-07 19:54:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const mensajes = [
-  "Hecha con HTML 💻",
-  "Diseñada con CSS 🎨",
-  "Animada con JavaScript ✨",
-  "Proyecto Escolar 🌼"
+  "Eres mi todo 💖",
+  "La flor siente tu amor 🌸",
+  "La magia está viva ✨",
+  "Gracias por tocarla 💜"
 ];
 
 let i = 0;
 const texto = document.getElementById("mensaje");
+const corazones = document.getElementById("corazones");
 
-setInterval(() => {
+function magia() {
   texto.textContent = mensajes[i];
   i = (i + 1) % mensajes.length;
-}, 2500);
+
+  for (let j = 0; j < 5; j++) {
+    const c = document.createElement("div");
+    c.className = "corazon";
+    c.textContent = "💖";
+    c.style.left = Math.random() * 100 + "%";
+    c.style.bottom = "120px";
+    corazones.appendChild(c);
+
+    setTimeout(() => c.remove(), 4000);
+  }
+}
